2f3bcadbb9f5f2b4f3f8c07b5d16bfd2d5fd7aa5
Fendika Edo Ferdinata <fendikaedo@gmail.com>
2024-12-04 15:04:27 +0700

    My Portofolio

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//Click Page About
+const pageAbout = document.getElementById('pageAbout');
+
+pageAbout.addEventListener('click', function () {
+    const contentAbout = document.getElementById('about');
+    contentAbout.append();
+});
+
+//Click Page About
+const pageProject = document.getElementById('pageProject');
+
+pageProject.addEventListener('click', function () {
+    const contentProject = document.getElementById('project');
+    contentProject.append()
+});
+
+//Infinite Scroll Logo
+const logo = document.querySelector('.logo').cloneNode(true);
+document.querySelector('.logo-container').appendChild(logo);

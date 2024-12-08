75fc277e4984048d625cf5e100be3e58defde9f1
Fendika Edo Ferdinata <fendikaedo@gmail.com>
2024-12-09 00:33:23 +0700

    My Portofolio

--- js/script.js
+++ js/script.js
@@ -1,19 +1,42 @@
-//Click Page About
-const pageAbout = document.getElementById('pageAbout');
+document.addEventListener("DOMContentLoaded", function () {
+  //! NAVBAR CONTENT
 
-pageAbout.addEventListener('click', function () {
-    const contentAbout = document.getElementById('about');
-    contentAbout.append();
+  //?Navbar Click Page About
+  const pageAbout = document.getElementById("pageAbout");
+
+  if (pageAbout) {
+    pageAbout.addEventListener("click", function (event) {
+      event.preventDefault();
+      const contentAbout = document.getElementById("about");
+      contentAbout.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  }
+
+  //?Navbar Click Page Project
+  const pageProject = document.getElementById("pageProject");
+
+  if (pageProject) {
+    pageProject.addEventListener("click", function (event) {
+      event.preventDefault();
+      const contentProject = document.getElementById("project");
+      contentProject.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  }
+
+
+
+  //!Button Click Description About
+  const nextAbout = document.getElementById("nextAbout");
+  const titleAbout = document.getElementById("titleAbout");
+
+  nextAbout.addEventListener("click", function () {
+    titleAbout.textContent = "Haloo";
+  });
+  
 });
-
-//Click Page About
-const pageProject = document.getElementById('pageProject');
-
-pageProject.addEventListener('click', function () {
-    const contentProject = document.getElementById('project');
-    contentProject.append()
-});
-
-//Infinite Scroll Logo
-// const logo = document.querySelector('.logo').cloneNode(true);
-// document.querySelector('.logo-container').appendChild(logo);

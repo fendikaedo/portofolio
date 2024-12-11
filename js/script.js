700fab35db6f71ac0d387f1b4903f8c33be1b770
Fendika Edo Ferdinata <fendikaedo@gmail.com>
2024-12-11 12:53:32 +0700

    My Portofolio

--- js/script.js
+++ js/script.js
@@ -29,7 +29,50 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
+  //?Navbar Click Page Project
+  const pageHistory = document.getElementById("pageHistory");
 
+  if (pageHistory) {
+    pageHistory.addEventListener("click", function (event) {
+      event.preventDefault();
+      const contentHistory = document.getElementById("history");
+      contentHistory.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  }
+
+  //?Link Social Media
+  //! Github
+  const github = document.getElementById('myGithub');
+  github.addEventListener('click', function() {
+    const linkGithub = `https://github.com/fendikaedo`;
+    window.open(linkGithub,'__blank');
+  });
+
+  //! Instagram
+  const instagram = document.getElementById('myInstagram');
+  instagram.addEventListener('click', function() {
+    const linkInstagram = `https://www.instagram.com/frdinata_`;
+    window.open(linkInstagram,'__blank');
+  });
+
+  //! Tiktok
+  const tiktok = document.getElementById('myTiktok');
+  tiktok.addEventListener('click', function() {
+    const linkTiktok = `https://www.tiktok.com/@fndkfrdinata`;
+    window.open(linkTiktok,'__blank');
+  });
+
+  //!Button Click Whatsapp
+  // const buttonWA = document.getElementById('whatsapp');
+
+  // buttonWA.addEventListener('click', function(){
+  //   const linkWA = document.querySelector('a');
+
+  //   linkWA.classList.add('href','https://wa.me/+62895608044646');
+  // })
 
   //!Button Click Description About
   const nextAbout = document.getElementById("nextAbout");
@@ -38,5 +81,4 @@ document.addEventListener("DOMContentLoaded", function () {
   nextAbout.addEventListener("click", function () {
     titleAbout.textContent = "Haloo";
   });
-  
 });

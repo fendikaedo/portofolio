d0c973083690b5379a2209626663d2fc2887464b
Fendika Edo Ferdinata <fendikaedo@gmail.com>
2024-12-26 17:00:14 +0700

    My Portofolio

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   AOS.refresh(); // Memperbarui posisi elemen AOS
 });
 
@@ -6,107 +6,128 @@ AOS.init({
   duration: 1000, // Durasi animasi (1 detik)
   once: true, // Animasi hanya dijalankan sekali
   mirror: false, // Jangan ulangi animasi ketika scroll ke atas
-  anchorPlacement: 'top-bottom', // Posisi anchor elemen
+  anchorPlacement: "top-bottom", // Posisi anchor elemen
 });
 
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar-container");
+  if (window.scrollY > 0) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+//!LOADING
+// document.addEventListener("DOMContentLoaded", () => {
+//   const loader = document.getElementById("loader");
+//   const body = document.querySelector("body");
+
+//   setTimeout(() => {
+//     loader.style.display = "none";
+//     body.style.display = "flex";
+//   }, 3000);
+// });
+
 //? TOGGLE BUTTON
-const navToggle = document.querySelector('.nav-toggle-off')
-const toggleButton = document.querySelectorAll('.nav-toggle-icon i');
-const toggleOn = document.getElementById('onToggle');
-const toggleOff = document.getElementById('offToggle');
-toggleButton.forEach(function(toggle){
-  toggle.addEventListener('click',function(){
-    if(toggle == toggleOn){
-      navToggle.classList.remove('nav-toggle-off');
-      navToggle.classList.add('nav-toggle-on');
-    }else if(toggle == toggleOff){
-      navToggle.classList.remove('nav-toggle-on');
-      navToggle.classList.add('nav-toggle-off');
+const navToggle = document.querySelector(".nav-toggle-off");
+const toggleButton = document.querySelectorAll(".nav-toggle-icon i");
+const toggleOn = document.getElementById("onToggle");
+const toggleOff = document.getElementById("offToggle");
+toggleButton.forEach(function (toggle) {
+  toggle.addEventListener("click", function () {
+    if (toggle == toggleOn) {
+      navToggle.classList.remove("nav-toggle-off");
+      navToggle.classList.add("nav-toggle-on");
+    } else if (toggle == toggleOff) {
+      navToggle.classList.remove("nav-toggle-on");
+      navToggle.classList.add("nav-toggle-off");
     }
   });
 });
 
-// document.addEventListener("DOMContentLoaded", function () {
+//! NAVBAR CONTENT
+const navLinks = [
+  { id: "pageHome", target: "home" },
+  { id: "pageAbout", target: "about" },
+  { id: "pageProject", target: "project" },
+  { id: "pageHistory", target: "history" },
+  { id: "pageContact", target: "contact" },
+];
 
-  //! NAVBAR CONTENT
-
-  //?Navbar Click Page About
-  const pageAbout = document.getElementById("pageAbout");
-
-  if (pageAbout) {
-    pageAbout.addEventListener("click", function (event) {
+navLinks.forEach(function (link) {
+  const page = document.getElementById(link.id);
+  if (page) {
+    page.addEventListener("click", function (event) {
       event.preventDefault();
-      const contentAbout = document.getElementById("about");
-      contentAbout.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
+      const targetPage = document.getElementById(link.target);
+      if (targetPage) {
+        targetPage.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }
     });
   }
+});
 
-  //?Navbar Click Page Project
-  const pageProject = document.getElementById("pageProject");
+//!BUTTON CONTACT ME
+const buttonContact = document.getElementById("btnContact");
+buttonContact.addEventListener("click", function () {
+  const pageContact = document.getElementById("contact");
+  pageContact.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+});
 
-  if (pageProject) {
-    pageProject.addEventListener("click", function (event) {
-      event.preventDefault();
-      const contentProject = document.getElementById("project");
-      contentProject.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    });
+//?Link Social Media
+//! Github
+const github = document.getElementById("myGithub");
+github.addEventListener("click", function () {
+  const linkGithub = `https://github.com/fendikaedo`;
+  window.open(linkGithub, "__blank");
+});
+
+//! Instagram
+const instagram = document.getElementById("myInstagram");
+instagram.addEventListener("click", function () {
+  const linkInstagram = `https://www.instagram.com/frdinata_`;
+  window.open(linkInstagram, "__blank");
+});
+
+//! Tiktok
+const tiktok = document.getElementById("myTiktok");
+tiktok.addEventListener("click", function () {
+  const linkTiktok = `https://www.tiktok.com/@fndkfrdinata`;
+  window.open(linkTiktok, "__blank");
+});
+
+//! Whatsapp
+function sendMessage() {
+  const url = `https://wa.me/62895608044646?text=${".input-name".value}${
+    ".input-message".value
+  }`;
+
+  window.open(url, "__blank");
+}
+
+//?CLICK THUMBNAIL IMAGE HISTORY
+const contentImage = document.querySelector(".content-history");
+const imageHistory = document.querySelector(".imageHistory");
+const thumbnail = document.querySelectorAll(".thumb");
+contentImage.addEventListener("click", function (e) {
+  if ((e.target.className = "thumb")) {
+    imageHistory.src = e.target.src;
   }
+});
+//?CLICK TAB CONTACT
+// const contactContainer = document.querySelector('.contact-container');
 
-  //?Navbar Click Page Project
-  const pageHistory = document.getElementById("pageHistory");
+// contactContainer.addEventListener('click',function(e){
+//   e.preventDefault();
 
-  if (pageHistory) {
-    pageHistory.addEventListener("click", function (event) {
-      event.preventDefault();
-      const contentHistory = document.getElementById("history");
-      contentHistory.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    });
-  }
+//   if (e.target.className == '.whatsapp') {
 
-  //?Link Social Media
-  //! Github
-  const github = document.getElementById('myGithub');
-  github.addEventListener('click', function() {
-    const linkGithub = `https://github.com/fendikaedo`;
-    window.open(linkGithub,'__blank');
-  });
-
-  //! Instagram
-  const instagram = document.getElementById('myInstagram');
-  instagram.addEventListener('click', function() {
-    const linkInstagram = `https://www.instagram.com/frdinata_`;
-    window.open(linkInstagram,'__blank');
-  });
-
-  //! Tiktok
-  const tiktok = document.getElementById('myTiktok');
-  tiktok.addEventListener('click', function() {
-    const linkTiktok = `https://www.tiktok.com/@fndkfrdinata`;
-    window.open(linkTiktok,'__blank');
-  });
-
-  //!Button Click Description About
-  const nextAbout = document.getElementById("nextAbout");
-  const titleAbout = document.getElementById("titleAbout");
-
-  nextAbout.addEventListener("click", function () {
-    titleAbout.textContent = "Haloo";
-  });
-
-
-
-  //?BUTTON SLIDE HISTORY
-  
-
-
-
-// });
+//   }
+// })
